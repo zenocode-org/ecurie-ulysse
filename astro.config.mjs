b75a4f861288b82ecffb17a9e4fddc7d00d9c110
Antoine <zenocode@pm.me>
2026-01-04 22:58:06 +0100

    Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,22 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import { webcore } from "webcoreui/integration";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), webcore(), react()],
   site: "https://energy-template.vbartalis.dev",
   output: "static",
   trailingSlash: "ignore",
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: "modern-compiler",
+        },
+      },
+    },
+  },
 });
